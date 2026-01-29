51022eb76457fc9d62ca4705492f4b5a214d6ade
dzt0062 <dzt0062@auburn.edu>
2026-01-28 21:34:06 -0500

    save3

--- HW1.R
+++ HW1.R
@@ -65,7 +65,9 @@ t[t !=2]
 #Data types
 
 mat1 <- matrix(data = c(1,2,3), nrow = 3, ncol =3)
+mat2 <- matrix(data = c("Doug", "Jade", "Jude"), nrow=3, ncol=3)
 
+mat2
 mat1
 
 mat1[1]
@@ -76,3 +78,40 @@ mat1[1,3]
 mat1[2,]
 
 #Data Frames
+
+df<- data.frame(mat1[,1], mat2[,1])
+df
+
+colnames(df) <-c("value", "name")
+df
+
+df[1]
+df[1,2]
+
+df[,"value"]
+
+df$value
+
+df$value[1]
+df$name[3]
+df$name[2:3]
+
+#subsetting
+
+df$value[df$name == "Jade"]
+
+df$value[!df$name %in% "Jade"]
+
+df$value[!df$name %in% c("Jade", "Jude")]
+
+
+subset(df, name=="Doug")
+
+df$log_value <- log(df$value)
+df
+
+
+
+
+
+
