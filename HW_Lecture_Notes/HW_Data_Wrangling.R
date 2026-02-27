21bbbe7beb64def0b232b540637c636f8ca7543f
dzt0062 <dzt0062@auburn.edu>
2026-02-26 20:37:15 -0500

    save

--- HW_Lecture_Notes/HW_Data_Wrangling.R
+++ HW_Lecture_Notes/HW_Data_Wrangling.R
@@ -1,8 +1,37 @@
 #seamless data wrangling
+#manipulating messy data
+
+library(tidyverse)
 
 microbiome.fungi <- read.csv("Data/Bull_richness.csv")
 str(microbiome.fungi)
 
-library(tidyverse)
+microbiome.fungi2 <- select(microbiome.fungi, SampleID, Crop, Compartment:Fungicide, richness)
 
-l
+head(filter(microbiome.fungi2, Treatment == "Conv."))
+head(filter(microbiome.fungi2, Treatment == "Conv." & Fungicide == "C"))
+head(filter(microbiome.fungi2, Sample == "A" | Sample == "B"))
+
+head(mutate(microbiome.fungi2, logRich = log(richness)))
+head(mutate(microbiome.fungi2, Crop_Treatment = paste(Crop, Treatment)))
+
+microbiome.fungi %>%
+  select(SampleID, Crop, Compartment:Fungicide, richness) %>% # selecting columns
+  filter(Treatment == "Conv.") %>% # subsetting to only include the conventional treatment
+  mutate(logRich = log(richness)) %>% # creating a new column of the log richness
+  head() # displaying the first six rows
+
+microbiome.fungi %>%
+  select(SampleID, Crop, Compartment:Fungicide, richness) %>% # selecting columns
+  filter(Treatment == "Conv.") %>% # subsetting to only include the conventional treatment
+  mutate(logRich = log(richness))%>% # creating a new column of the log richness
+  summarise(Mean.rich = mean(logRich))
+
+microbiome.fungi %>%
+  select(SampleID, Crop, Compartment:Fungicide, richness) %>% # selecting columns
+  filter(Treatment == "Conv.") %>% # subsetting to only include the conventional treatment
+  mutate(logRich = log(richness)) %>% # creating a new column of the log richness
+  summarise(Mean.rich = mean(logRich), # calculating the mean richness, stdeviation, and standard error
+            n = n(), 
+            sd.dev = sd(logRich)) %>%
+  mutate(std.err = sd.dev/sqrt(n))
