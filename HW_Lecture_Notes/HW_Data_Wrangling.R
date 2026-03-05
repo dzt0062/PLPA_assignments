95974b66ac515b85fd85d88b616dbd1bcc17e799
dzt0062 <dzt0062@auburn.edu>
2026-03-05 11:29:35 -0500

    saved

--- HW_Lecture_Notes/HW_Data_Wrangling.R
+++ HW_Lecture_Notes/HW_Data_Wrangling.R
@@ -8,6 +8,17 @@ str(microbiome.fungi)
 
 microbiome.fungi2 <- select(microbiome.fungi, SampleID, Crop, Compartment:Fungicide, richness)
 
+#head is the first 6 rows
+#mutate() adds new variables or modifies existing variables in a dataset.
+#select() chooses specific columns (variables) from a dataset.
+#filter() keeps only the rows that meet specified conditions.
+#The pipe %>% passes the result of one function directly into the next function, making code easier to read and chain together.
+#summarise() calculates summary statistics for variables in a dataset.
+#group_by() groups data by one or more variables so operations like summarise() are performed within each group.
+#Joining combines two datasets by matching rows based on a common key variable.
+#Pivotting reshaping data between wide and long formats using functions like pivot_longer() and pivot_wider().
+#Integration with plotting, Tidyverse data manipulation works directly with ggplot2, allowing cleaned or summarized data to be piped straight into plotting functions for visualization.
+
 head(filter(microbiome.fungi2, Treatment == "Conv."))
 head(filter(microbiome.fungi2, Treatment == "Conv." & Fungicide == "C"))
 head(filter(microbiome.fungi2, Sample == "A" | Sample == "B"))
@@ -66,6 +77,9 @@ microbiome.fungi %>%
   group_by(Treatment, Fungicide) %>% # grouping by treatment and fungicide to later calculate summary stats by group
   summarise(Mean = mean(richness)) # calculates the mean per Treatment and Fungicide
 
+#Pivoting data
+#Wide: one row per entity, many columns for measurements.
+#Long: multiple rows per entity, measurements stacked in rows with a variable indicating type/time.
 microbiome.fungi %>%
   select(SampleID, Crop, Compartment:Fungicide, richness) %>% # selecting columns  filter(Class == "Sordariomycetes") %>%
   group_by(Treatment, Fungicide) %>% # grouping by treatment and fungicide to later calculate summary stats by group
